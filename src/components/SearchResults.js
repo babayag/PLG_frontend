5a423f58a8c3a76fa176b4453cd486dc55dcaa60
nouboussi junior <nouboussijunior@gmail.com>
2019-02-13 19:50:24 +0100

    Favicon Title Changed

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -50,29 +50,11 @@ export class SearchResults extends Component {
 
 
     countSources(){
-        if(this.props.emailList.data.length-1 <= 0){
+        if(this.props.emailList.data.length === 0){
            return <span>No Email Address Found.</span> 
 
-        } else if(this.props.emailList.data.length-1 === 1){
-            return <span>1 Email Address Found.  </span>
-        } else{
-            return <span> {this.props.emailList.data.length} Email Addresses Found. </span>
-        }
-    }
-
-    isOdd(num) { 
-        return num % 2;
-    }
-    
-    render() {  /* We remove 1 to the length because the last object in the json 
-        files is not an object we want to read */
-        const resultat = this.props.emailList; //this.state.list
-        return (
-            <div>
-                <div class="emailResult numberOfEmails">
-                    <p class="text-center numberOfEmails1">
-                        {this.countSources()} 
-                        
+        } else if(this.props.emailList.data.length === 1){
+            return <span>1 Email Address Found.  
                         <CSVLink
                             filename={"my-file.csv"}
                             className="btn exportBtn"
@@ -87,7 +69,41 @@ export class SearchResults extends Component {
                             <span className="numberInExportBtn">
                                 {this.props.emailList.data.length}
                             </span> 
-                        </CSVLink>                      
+                        </CSVLink>  
+                   </span>
+        } else{
+            return <span> {this.props.emailList.data.length} Email Addresses Found. 
+                        <CSVLink
+                            filename={"my-file.csv"}
+                            className="btn exportBtn"
+                            target="_blank"
+                            data={this.props.emailList.data}
+                            asyncOnClick={true}
+                            onClick={
+                                this.exportDatasToCsv
+                            }
+                        >    
+                            Export
+                            <span className="numberInExportBtn">
+                                {this.props.emailList.data.length}
+                            </span> 
+                        </CSVLink>  
+                    </span>
+        }
+    }
+
+    isOdd(num) { 
+        return num % 2;
+    }
+    
+    render() {  /* We remove 1 to the length because the last object in the json 
+        files is not an object we want to read */
+        const resultat = this.props.emailList; //this.state.list
+        return (
+            <div>
+                <div class="emailResult numberOfEmails">
+                    <p class="text-center numberOfEmails1">
+                        {this.countSources()}                     
                     </p>
                     
                 </div>
