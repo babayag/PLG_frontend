7e6ec7a12a67ff2fdf76ca06671db8dcffeae45e
nouboussi junior <nouboussijunior@gmail.com>
2019-02-22 15:34:04 +0100

    Cold Email Course in Export Page

---
+++ src/components/BulkSearch.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { NavBar } from "./NavBar";
+
+export class BulkSearch extends Component {
+    render() {
+        return (
+            <div className="">
+                <NavBar/>
+                <div class="">
+                    Hooohe
+                </div>
+
+            </div>
+        );
+    }
+}
