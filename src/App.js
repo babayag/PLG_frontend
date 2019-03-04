9a4527ceb80eaf7229023f50448fbe07d7efc127
kevin-it <ngaleuabel@gmail.com>
2019-03-04 10:20:35 +0100

    Design 404 page

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import { Dashboard } from "./components/Dashboard";
 import { Finder } from "./components/Finder";
 import { DashboardLead } from "./components/DashboardLead";
 import { DashboardFinder } from "./components/DashboardFinder";
+import { Page404 } from "./components/Page404";
 // import  TestIt  from "./components/TestIt";
 
 import './App.css';
@@ -91,12 +92,16 @@ class App extends Component {
           <BrowserRouter>
             <Route exact path={"/dashboard/finder"} component={DashboardFinder}  />
           </BrowserRouter>
+
           <BrowserRouter>
             <Route exact path={"/finder"} component={Finder}  />
           </BrowserRouter>
 
-        </Provider>
+          <BrowserRouter>
+            <Route exact path={"/404"} component={Page404}  />
+          </BrowserRouter>
 
+        </Provider>
 
       </div>
 
