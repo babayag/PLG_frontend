d2ecb9a0bb23a825c28042ce518c6a9ad4528095
babayag <ironclock6@gmail.com>
2019-05-29 14:51:09 +0100

    get all forfaits using redux

---
+++ src/reducers/forfait/forfait.js
@@ -0,0 +1,21 @@
+const initialState = {
+    isForfaitLoading : true,
+    forfaits : [],
+}
+
+export default function forfaits(state=initialState, action) {
+
+    switch (action.type) {
+        
+        case 'FETCH_FORFAIT':
+            return {...state, forfaits : action.forfaits , isForfaitLoading : false };
+  
+        default:
+            return state;
+    }
+  }
+
+
+
+
+
